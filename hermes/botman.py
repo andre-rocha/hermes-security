83a13be559ffaacec53b86c8145a340cc43953f2
André Carneiro <andreluizmrcarneiro@gmail.com>
2017-07-21 08:07:11 -0300

    UI and face detect

--- hermes/botman.py
+++ hermes/botman.py
@@ -12,9 +12,9 @@ bot = None
 inline_id = None
 
 def send_picture(path, caption=''):
-    photo = open(path, 'rb')
     for u in config['AUTH_USERS']:
         print('[*] Sending %s picture to %s' % (path.split('/')[-1], u))
+        photo = open(path, 'rb')
         bot.sendPhoto(u, photo, caption=caption)
 
 
@@ -33,6 +33,7 @@ def start():
     global bot
     bot = telepot.Bot(config["TOKEN"])
     config['AUTH_USERS'] = [config['SUPER_USER']]
+    config['INVITE'] = []
 
     MessageLoop(bot, {
             'chat': on_chat_message,
@@ -45,11 +46,19 @@ def on_chat_message(msg):
     print(msg)
     content_type, chat_type, chat_id = telepot.glance(msg)
     print('Chat:', content_type, chat_type, chat_id)
-    if chat_id != config['SUPER_USER']:
-        print('UNAUTHORIZED ACCESS...')
-        return
 
     if content_type != 'text':
+        return
+
+    try:
+        if msg['text'][0:5] == '/auth':
+            config['INVITE'].append(chat_id)
+            return
+    except:
+        pass
+
+    if chat_id != config['SUPER_USER']:
+        print('UNAUTHORIZED ACCESS...')
         return
 
     command = msg['text'].lower()
@@ -68,7 +77,27 @@ def on_chat_message(msg):
             ]
         ])
         global inline_id
-        inline_id = bot.sendMessage(chat_id, 'CHOOSE MUTHAFUCKA:', reply_markup=keyboard)
+        inline_id = bot.sendMessage(chat_id, 'Settings', reply_markup=keyboard)
+
+    elif command == '/list_invites':
+        kb_list = [InlineKeyboardButton(text=str(u), callback_data="auth_%s" % u) for u in config['INVITE']]
+        kb_auth = [InlineKeyboardButton(text=str(u), callback_data="auth_%s" % u) for u in config['AUTH_USERS'] if u != config['SUPER_USER']]
+
+        if len(kb_auth) > 0:
+            keyboard_auth = InlineKeyboardMarkup(inline_keyboard=[
+                kb_auth,
+            ])
+            bot.sendMessage(chat_id, 'Authorized chats :', reply_markup=keyboard_auth)
+
+        if len(kb_list) > 0:
+            keyboard_list = InlineKeyboardMarkup(inline_keyboard=[
+                kb_list,
+            ])
+            global inline_id
+            inline_id = bot.sendMessage(chat_id, 'Waiting List:', reply_markup=keyboard_list)
+            config['INVITE'] = []
+        else:
+            bot.sendMessage(chat_id, 'No chats in the waiting list...')
 
 
 def on_callback_query(msg):
@@ -137,5 +166,15 @@ def on_callback_query(msg):
         cmd_func()
         return
 
+    else:
+        if query_data.startswith('auth_'):
+            if int(query_data[5:]) in config['AUTH_USERS']:
+                config['AUTH_USERS'].remove(int(query_data[5:]))
+                bot.answerCallbackQuery(query_id, '%s authorized.' % query_data[5:])
+                if int(query_data[5:]) in config['INVITE']:
+                    config['INVITE'].remove(int(query_data[5:]))
+            else:
+                config['AUTH_USERS'].append(int(query_data[5:]))
+                bot.answerCallbackQuery(query_id, '%s unauthorized.' % query_data[5:])
 
 
